ad30ea33aecb49a2d633a6e3bcfa2e8f76a15c9c
kehlin swain <kehlinswain@kehlins-MacBook-Pro.local>
2012-11-22 12:17:04 -0600

    Initial Commit

---
+++ Calculator 2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Calculator 2
+//
+//  Created by kehlin swain on 11/22/12.
+//  Copyright (c) 2012 kehlin swain. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
